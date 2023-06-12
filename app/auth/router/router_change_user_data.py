c509f3f9e3aec64d28da45496f1b51d908f58b3f
inabatakanova <akanovainka@gmail.com>
2023-06-12 23:59:36 +0600

    feature: edit user data

---
+++ app/auth/router/router_change_user_data.py
@@ -0,0 +1,43 @@
+from fastapi import Depends, HTTPException, status
+
+from app.utils import AppModel
+
+from ..service import Service, get_service
+from . import router
+
+
+class UpdateUserRequest(AppModel):
+    email: str
+    name: str
+    city: str
+    phone: str
+    password: str
+
+
+class UpdateUserResponse(AppModel):
+    email: str
+    name: str
+    city: str
+    phone: str
+
+
+@router.patch(
+    "/auth/users/me",
+    status_code=status.HTTP_200_OK,
+    response_model=UpdateUserResponse
+)
+def update_user(
+    user_id: int,
+    input: UpdateUserRequest,
+    svc: Service = Depends(get_service)
+) -> dict[str, str]:
+    user = svc.repository.get_user_by_id(user_id)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found."
+        )
+
+    updated_user = svc.repository.update_user(user_id, input.dict())
+
+    return UpdateUserResponse(email=updated_user.email)
