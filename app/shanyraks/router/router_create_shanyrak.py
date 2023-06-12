7262d53ccd99dfa73d6817d66d6e838ea45ed69d
inabatakanova <akanovainka@gmail.com>
2023-06-13 02:28:15 +0600

    shanyraks created

--- app/shanyraks/router/router_create_shanyrak.py
+++ app/shanyraks/router/router_create_shanyrak.py
@@ -8,17 +8,17 @@ from ..service import Service, get_service
 from . import router
 
 
-class CreateTweetRequest(AppModel):
+class CreateShanyrakRequest(AppModel):
     content: str
 
 
 @router.post("/")
-def create_tweet(
-    input: CreateTweetRequest,
+def create_shanyrak(
+    input: CreateShanyrakRequest,
     jwt_data: JWTData = Depends(parse_jwt_user_data),
     svc: Service = Depends(get_service),
 ) -> dict[str, str]:
     user_id = jwt_data.user_id
-    svc.repository.create_tweet({"user_id": user_id, "content": input.content})
+    svc.repository.create_shanyrak({"user_id": user_id, "content": input.content})
 
     return Response(status_code=200)
