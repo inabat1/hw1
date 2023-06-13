5caab93a7355058a4818fc16920df1997dbc9efb
inabatakanova <akanovainka@gmail.com>
2023-06-13 14:16:10 +0600

    shanyrak: get

--- app/shanyraks/repository/repository.py
+++ app/shanyraks/repository/repository.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import List, Any
+from typing import List, Any, Optional
 
 from bson.objectid import ObjectId
 from pymongo.database import Database
@@ -12,8 +12,10 @@ class ShanyrakRepository:
     def create_shanyrak(self, user_id: str, data: dict[str, Any]):
         data["user_id"] = ObjectId(user_id)
         shanyrak = self.database["shanyraks"].insert_one(data)
-
         return shanyrak.inserted_id
 
-    def get_shanyrak(self, shanyrak_id:str):
-        self.database["shanyraks"].find_one
+    def get_shanyrak(self, shanyrak_id:str, user_id:str)-> Optional[dict]:
+        user = self.database["shanyraks"].find_one(
+            {"_id": ObjectId(shanyrak_id), "user_id": ObjectId(user_id)}
+        )
+        return user
