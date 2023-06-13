e366038c8bb6deae9c559dab00b7ff814ee5b4bb
inabatakanova <akanovainka@gmail.com>
2023-06-13 15:16:32 +0600

    create shanyrak

--- app/shanyraks/router/router_create_shanyrak.py
+++ app/shanyraks/router/router_create_shanyrak.py
@@ -3,7 +3,8 @@ from fastapi import Depends, Response
 from app.auth.adapters.jwt_service import JWTData
 from app.auth.router.dependencies import parse_jwt_user_data
 from app.utils import AppModel
-from typing import Any, Field
+from typing import Any 
+from pydantic import Field
 from ..service import Service, get_service
 from . import router
 
