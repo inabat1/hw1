d2071b04386272e3a2590fc83d91f15be02a73fc
inabatakanova <akanovainka@gmail.com>
2023-06-14 17:00:47 +0600

    add media trial

--- app/shanyraks/router/router_add_media.py
+++ app/shanyraks/router/router_add_media.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, Response
+from fastapi import Depends, Response, UploadFile
 
 from app.auth.adapters.jwt_service import JWTData
 from app.auth.router.dependencies import parse_jwt_user_data
@@ -20,7 +20,14 @@ class CreateShanyrakRequest(AppModel):
 
 
 @router.post("/{shanyrak_id:str}/media")
-def add_media():
+def add_media(
+    file: UploadFile,
+    svc: Service = Depends(get_service),
+):
+    """
+    file.filename: str - Название файла
+    file.file: BytesIO - Содержимое файла
+    """
+    url = svc.s3_service.upload_file(file.file, file.filename)
     
-
-    return Response(status_code=200)
+    return {"msg": url}
