2049720f5f09a1acc9275de3b1e2886edb25ce94
inabatakanova <akanovainka@gmail.com>
2023-06-16 19:28:21 +0600

    render file added

--- app/shanyraks/router/router_create_shanyrak.py
+++ app/shanyraks/router/router_create_shanyrak.py
@@ -1,9 +1,9 @@
-from fastapi import Depends, Response
 
+from fastapi import Depends, Response
 from app.auth.adapters.jwt_service import JWTData
 from app.auth.router.dependencies import parse_jwt_user_data
 from app.utils import AppModel
-from typing import Any, List
+from typing import Any
 from pydantic import Field
 from ..service import Service, get_service
 from . import router
@@ -20,14 +20,20 @@ class CreateShanyrakRequest(AppModel):
 
 class CreateShanyrakResponse(AppModel):
     id: Any = Field(alias="_id")
+   
+    
 
 @router.post("/", response_model=CreateShanyrakResponse)
 def create_shanyrak(
     input: CreateShanyrakRequest,
     jwt_data: JWTData = Depends(parse_jwt_user_data),
     svc: Service = Depends(get_service),
-)-> dict[str,str]:
-    shanyrak_id=svc.repository.create_shanyrak(jwt_data.user_id, input.dict())
+) -> dict[str,str]:
+    coordinates = svc.here_service.get_coordinates(input.address)
+    
+    user_id = jwt_data.user_id
+    shanyrak_data = input.dict()| coordinates
+
+    shanyrak_id = svc.repository.create_shanyrak(jwt_data.user_id, shanyrak_data)
 
     return CreateShanyrakResponse(id=shanyrak_id)
-
